7f9b72aaa2892710ca8cf2171f3ef7ca824d87ea
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-09-04 13:00:01 +0200

    tables in db + jdbc

--- sql-scripts/04-setup-spring-security-demo-database-plaintext.sql
+++ sql-scripts/04-setup-spring-security-demo-database-plaintext.sql
@@ -1,46 +1,29 @@
-USE `employee_directory`;
 
-DROP TABLE IF EXISTS `authorities`;
-DROP TABLE IF EXISTS `users`;
+-- Create the 'users' table
+CREATE TABLE users (
+  username varchar(50) NOT NULL,
+  password varchar(50) NOT NULL,
+  enabled smallint NOT NULL,  -- Use smallint instead of tinyint
+  PRIMARY KEY (username)
+);
 
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `enabled` tinyint NOT NULL,
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Inserting data for table `users`
---
-
-INSERT INTO `users` 
+-- Insert data into the 'users' table
+INSERT INTO users 
 VALUES 
 ('john','{noop}test123',1),
 ('mary','{noop}test123',1),
 ('susan','{noop}test123',1);
 
+-- Create the 'authorities' table
+CREATE TABLE authorities (
+  username varchar(50) NOT NULL,
+  authority varchar(50) NOT NULL,
+  CONSTRAINT authorities_pkey PRIMARY KEY (username, authority),  -- Define primary key constraint
+  CONSTRAINT fk_authorities_users FOREIGN KEY (username) REFERENCES users (username)
+);
 
---
--- Table structure for table `authorities`
---
-
-CREATE TABLE `authorities` (
-  `username` varchar(50) NOT NULL,
-  `authority` varchar(50) NOT NULL,
-  UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
-  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Inserting data for table `authorities`
---
-
-INSERT INTO `authorities` 
+-- Insert data into the 'authorities' table
+INSERT INTO authorities 
 VALUES 
 ('john','ROLE_EMPLOYEE'),
 ('mary','ROLE_EMPLOYEE'),
@@ -48,5 +31,9 @@ VALUES
 ('susan','ROLE_EMPLOYEE'),
 ('susan','ROLE_MANAGER'),
 ('susan','ROLE_ADMIN');
+
+-- Comment on the database
+COMMENT ON DATABASE postgres
+    IS 'default administrative connection database';
 
 
